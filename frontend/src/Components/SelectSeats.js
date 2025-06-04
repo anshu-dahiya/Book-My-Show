84878d5f830415e072b15712621a44ca1a4fd872
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-06-04 13:14:26 +0530

    selectSeat and selectInput with css

--- frontend/src/Components/SelectSeats.js
+++ frontend/src/Components/SelectSeats.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { seats } from '../data'
+import SeatInputs from './SeatInputs'
+import '../Css/SelectSeats.css'
 
 const SelectSeats = () => {
   return (
-    <div>SelectSeats</div>
+    <div className='SS_wrapper'>
+      <h1 className='SS_heading'>Select Seats : </h1>
+
+      <div className='SS_main_container'>
+        {seats.map((element,index) => {
+          return(
+            <SeatInputs text={element} key={index} />
+          )
+        })}
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,5 +1,7 @@
 let moviesList = ['Stolen','Housefull 5','Bombay (2025)','Interrogation','Kapkapiii'];
 let slots = ['10:00 AM','01:00 PM','03:00 PM','08:00 AM','11:00 PM'];
+let seats = ['A1','A2','A3','A4','D1','D2'];
 
 exports.moviesList = moviesList;
 exports.slots = slots;
+exports.seats = seats;
