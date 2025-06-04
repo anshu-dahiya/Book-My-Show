a1e7b1cc6fe48e4a0137c9c366650aac104401c7
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-06-04 19:02:59 +0530

    LastBookingDetail and its css

--- frontend/src/Components/LastBookingDetails.js
+++ frontend/src/Components/LastBookingDetails.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { seats } from '../data'
+import '../Css/LastBookingDetails.css'
 
 const LastBookingDetails = () => {
   return (
-    <div>LastBookingDetails</div>
+    <div className='LBD_main_container'>
+      <h2 className='LBD_heading'>Last Booking :-</h2>
+      
+      <div className='seats_container'>
+        <p className='seats_heading'>Seats:</p>
+        <ul className='seats'>
+          {seats.map((seat,index) => (
+            <li className='seat_value' key={index}>
+              (seat) : 0;
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <p className='slot' style={{textAlign:'left'}}>Slot: <span>00:00 AM</span></p>
+      <p className='movie'>Movie: <span>Stolen</span></p>
+
+    </div>
   )
 }
 
