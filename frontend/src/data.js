39cdbcc68dca452e151e23c5dd98cb5e50935d22
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-06-08 12:43:31 +0530

    data adeded

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,8 +1,9 @@
-let moviesList = ['Stolen','Housefull 5','Bombay (2025)','Interrogation','Kapkapiii','Sikandar',
-    'The Diplomat',
+let moviesList = ['Stolen', 'Housefull 5', 'Bombay (2025)', 'Interrogation', 'Kapkapiii',
+    'Sikandar', 'The Diplomat', 'Despatch', 'Tourist Family', 'Retro', 'Sector 36',
+    'Until Dawn',
 ];
-let slots = ['10:00 AM','01:00 PM','03:00 PM','08:00 AM','11:00 PM'];
-let seats = ['A1','A2','A3','A4','D1','D2'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', '08:00 AM', '11:00 PM'];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
 
 exports.moviesList = moviesList;
 exports.slots = slots;
